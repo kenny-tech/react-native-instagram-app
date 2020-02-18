5a4bb4303a05e1455ce0d381970e58a7f915d257
kenny-tech <kennyemma2008@gmail.com>
2020-02-18 05:31:01 +0100

    Register user

--- App.js
+++ App.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { f, auth, database } from './config/config';
 
 export default function App() {
+
+  useEffect(() => {
+    registerUser('test@gmail.com', 'test123!');
+  });
+
+  const registerUser = (email, password) => {
+    console.log('email: ', email, 'password: ', password);
+    auth.createUserWithEmailAndPassword(email, password)
+    .then((userObj) => console.log(email, password, userObj))
+    .catch((error) => console.log('error logging in', error));
+  }
+
   return (
     <View style={styles.container}>
       <Text>React native instagram app</Text>
+      <Text>Welcome to react native!</Text>
     </View>
   );
 }
